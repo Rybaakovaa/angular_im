756e14f65c8761b01db8652d6cae9d15dad09cf8
aapavl <an29pavl@gmail.com>
2024-05-22 09:49:43 +0300

    fix backend

--- backend/migrations/20221128150925-addUser.js
+++ backend/migrations/20221128150925-addUser.js
@@ -4,7 +4,7 @@ module.exports = {
     async up(db, client) {
         const salt = crypto.randomBytes(128).toString('base64');
         const passwordHash = crypto.pbkdf2Sync('12345678', salt, 1, 128, 'sha1').toString('base64');
-        await db.collection('users').insert({
+        await db.collection('users').insertOne({
             firstName: 'Тест',
             lastName: 'Тестов',
             fatherName: 'Тестовович',
