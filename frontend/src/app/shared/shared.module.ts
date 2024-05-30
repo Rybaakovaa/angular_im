64267d9d9dd392286d92b352dd9b96537e4089fa
paa <rybaakovaa@gmai.com>
2024-05-30 20:11:52 +0300

    L3 29:06

--- frontend/src/app/shared/shared.module.ts
+++ frontend/src/app/shared/shared.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {PasswordRepeatDirective} from "./directives/password-repeat.directive";
+import { ProductCardComponent } from './components/product-card/product-card.component';
 
 
 
 @NgModule({
   declarations: [
-    PasswordRepeatDirective
+    PasswordRepeatDirective,
+    ProductCardComponent
   ],
   imports: [
     CommonModule
