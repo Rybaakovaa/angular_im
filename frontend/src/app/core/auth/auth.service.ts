632c5d921bf5d630776e5b8b64837a00252665f9
paa <rybaakovaa@gmai.com>
2024-06-10 21:26:02 +0300

    L8 43:22

--- frontend/src/app/core/auth/auth.service.ts
+++ frontend/src/app/core/auth/auth.service.ts
@@ -44,6 +44,16 @@ export class AuthService {
     throw throwError(() => 'Can not find token');
   }
 
+  refresh(): Observable<DefaultResponseType | LoginResponseType> {
+    const tokens = this.getTokens();
+    if (tokens && tokens.refreshToken) {
+      return this.http.post<DefaultResponseType | LoginResponseType>(environment.api + 'refresh', {
+        refreshToken: tokens.refreshToken
+      })
+    }
+    throw throwError(() => 'Can not find token');
+  }
+
   public getIsLoggedIn() {
     return this.isLogged;
   }
