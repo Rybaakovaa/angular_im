b7cc5f70aa3c59a1a98a5c7dba52552db088785c
paa <rybaakovaa@gmai.com>
2024-06-14 22:20:32 +0300

    end L12 all project

--- frontend/src/app/shared/layout/header/header.component.ts
+++ frontend/src/app/shared/layout/header/header.component.ts
@@ -1,14 +1,17 @@
-import {Component, Input, OnInit} from '@angular/core';
-import {CategoryType} from "../../../../types/category.type";
+import {Component, HostListener, Input, OnInit} from '@angular/core';
 import {AuthService} from "../../../core/auth/auth.service";
 import {DefaultResponseType} from "../../../../types/default-response.type";
 import {MatSnackBar} from "@angular/material/snack-bar";
 import {HttpErrorResponse} from "@angular/common/http";
 import {Router} from "@angular/router";
-import {visitAll} from "@angular/compiler";
 import {CategoryWithTypeType} from "../../../../types/category-with-type.type";
 import {CartService} from "../../services/cart.service";
-import {count} from "rxjs";
+import {debounceTime} from "rxjs";
+import {ProductService} from "../../services/product.service";
+import {ProductType} from "../../../../types/product.type";
+import {environment} from "../../../../environments/environment";
+import {FormControl} from "@angular/forms";
+import {LoaderService} from "../../services/loader.service";
 
 @Component({
   selector: 'app-header',
@@ -17,6 +20,12 @@ import {count} from "rxjs";
 })
 export class HeaderComponent implements OnInit {
 
+  // переменные для поиска
+  products: ProductType[] = [];
+  serverStaticPath: string = environment.serverStaticPath;
+  showedSearch: boolean = false;
+  searchField = new FormControl();
+
   count: number = 0;
   isLogged: boolean = false;
   @Input() categories: CategoryWithTypeType[] = [];
@@ -24,22 +33,44 @@ export class HeaderComponent implements OnInit {
   constructor(private authService: AuthService,
               private _snackBar: MatSnackBar,
               private router: Router,
+              private loaderService: LoaderService,
+              private productService: ProductService,
               private cartService: CartService) {
     this.isLogged = this.authService.getIsLoggedIn();
   }
 
   ngOnInit(): void {
+    // подписываемся на изменения в поиске
+    this.searchField.valueChanges
+      .pipe(
+        // задержка для частых (быстрее 500мс) запросов
+        debounceTime(500)
+      )
+      .subscribe(value => {
+          // делаем запрос начиная с 2х символов
+          if (value && value.length > 2) {
+            this.productService.searchProducts(value)
+              .subscribe((data: ProductType[]) => {
+                this.products = data;
+                this.showedSearch = true;
+              });
+          } else {
+            this.products = [];
+          }
+      });
+
+
     this.authService.isLogged$.subscribe((isLoggedIn: boolean) => {
       this.isLogged = isLoggedIn;
     });
 
     this.cartService.getCartCount()
-      .subscribe((data: {count: number} | DefaultResponseType) => {
+      .subscribe((data: { count: number } | DefaultResponseType) => {
         if ((data as DefaultResponseType).error) {
           // ...
           throw new Error((data as DefaultResponseType).message);
         }
-        this.count = (data as {count: number}).count;
+        this.count = (data as { count: number }).count;
       });
 
     // подписываемся на измененеия кол-ва товаров в корзине после запросов
@@ -76,4 +107,34 @@ export class HeaderComponent implements OnInit {
     this.router.navigate(['/']);
   }
 
+
+  // функционал для блока поиска
+  // changedSearchValue(newValue: string) {
+  //   this.searchValue = newValue;
+  //
+  //   // делаем запрос начиная с 2х символов
+  //   if (this.searchValue && this.searchValue.length > 2) {
+  //     this.productService.searchProducts(this.searchValue)
+  //       .subscribe((data: ProductType[]) => {
+  //         this.products = data;
+  //         this.showedSearch = true;
+  //       });
+  //   } else {
+  //     this.products = [];
+  //   }
+  // }
+
+  selectProduct(url: string) {
+    this.router.navigate(['/products/' + url]);
+    // this.searchValue = '';
+    this.searchField.setValue('');
+    this.products = [];
+  }
+
+  @HostListener('document: click', ['$event'])
+  click(event: Event) {
+    if (this.showedSearch && (event.target as HTMLElement).className.indexOf('search-product') === -1) {
+      this.showedSearch = false;
+    }
+  }
 }
--- frontend/src/app/shared/shared.module.ts
+++ frontend/src/app/shared/shared.module.ts
@@ -6,6 +6,8 @@ import {RouterModule} from "@angular/router";
 import {FormsModule} from "@angular/forms";
 import { CategoryFilterComponent } from './components/category-filter/category-filter.component';
 import { CountSelectorComponent } from './components/count-selector/count-selector.component';
+import { LoaderComponent } from './components/loader/loader.component';
+import {MatProgressSpinnerModule} from "@angular/material/progress-spinner";
 
 
 
@@ -14,18 +16,21 @@ import { CountSelectorComponent } from './components/count-selector/count-select
     PasswordRepeatDirective,
     ProductCardComponent,
     CategoryFilterComponent,
-    CountSelectorComponent
+    CountSelectorComponent,
+    LoaderComponent
   ],
   imports: [
     CommonModule,
     RouterModule,
+    MatProgressSpinnerModule,
     FormsModule
   ],
   exports: [
     PasswordRepeatDirective,
     ProductCardComponent,
     CountSelectorComponent,
-    CategoryFilterComponent
+    CategoryFilterComponent,
+    LoaderComponent
   ]
 })
 export class SharedModule { }
